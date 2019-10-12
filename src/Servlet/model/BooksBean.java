85a8eeb519659ded61ff2f6d1048888def020fec
Yang shen <614989768@qq.com>
2019-10-12 20:46:38 +0800

    change bookbean to builder

--- src/Servlet/model/BooksBean.java
+++ src/Servlet/model/BooksBean.java
@@ -1,7 +1,11 @@
 package Servlet.model;
 
-import java.sql.Date;
+import java.util.Date;
 
+/**
+ *
+ * Bookbean 使用builder构建
+ */
 public class BooksBean {
     private String Bnum;
     private String Bname;
@@ -13,75 +17,91 @@ public class BooksBean {
     private Date Btime;
     private boolean Bstatus;
 
-    public String getBnum() {
-        return Bnum;
+    public static class Builder{
+        private String Bnum;
+        private String Bname;
+        private String UID;
+        private String Bimage;
+        private String Btype;
+        private String Bdiscription;
+        private String Bprice;
+        private Date Btime;
+        private boolean Bstatus;
+
+        public Builder Bnum(String Bnum){
+            this.Bnum=Bnum;
+            return this;
+        }
+        public Builder Bname(String Bname){
+            this.Bname=Bname;
+            return this;
+        }
+        public Builder UID(String UID){
+            this.UID=UID;
+            return this;
+        }
+        public Builder Bimage(String Bimage){
+            this.Bimage=Bimage;
+            return this;
+        }
+        public Builder Btype(String Btype){
+            this.Btype=Btype;
+            return this;
+        }
+        public Builder Bdiscription(String Bdiscription){
+            this.Bdiscription=Bdiscription;
+            return this;
+        }
+        public Builder Bprice(String Bprice){
+            this.Bprice=Bprice;
+            return this;
+        }
+        public Builder Btime(Date Btime){
+            this.Btime=Btime;
+            return this;
+        }
+        public Builder Bstatus(boolean Bstatus){
+            this.Bstatus=Bstatus;
+            return this;
+        }
+
+        private BooksBean build(){
+            return new BooksBean(this);
+        }
+    }
+    private BooksBean(Builder builder){
+        Bnum=builder.Bnum;
+        Bname=builder.Bname;
+        UID=builder.UID;
+        Bimage=builder.Bimage;
+        Btype=builder.Btype;
+        Bdiscription=builder.Bdiscription;
+        Bprice=builder.Bprice;
+        Btime=builder.Btime;
+        Bstatus=builder.Bstatus;
     }
 
-    public void setBnum(String bnum) {
-        Bnum = bnum;
+    @Override
+    public String toString() {
+        return "BooksBean{" +
+                "Bnum='" + Bnum + '\'' +
+                ", Bname='" + Bname + '\'' +
+                ", UID='" + UID + '\'' +
+                ", Bimage='" + Bimage + '\'' +
+                ", Btype='" + Btype + '\'' +
+                ", Bdiscription='" + Bdiscription + '\'' +
+                ", Bprice='" + Bprice + '\'' +
+                ", Btime=" + Btime +
+                ", Bstatus=" + Bstatus +
+                '}';
     }
+    /*
+    public static void main(String[] args) {
+        BooksBean a =new Builder().Bimage("image").Bdiscription("discription").Bname("name").Bnum("num").UID("uid").Btype("type").Bprice("price").Btime(new Date())
+                .Bstatus(true).build();
+        System.out.println(a);
+    }
+     */
 
-    public String getBname() {
-        return Bname;
-    }
 
-    public void setBname(String bname) {
-        Bname = bname;
-    }
-
-    public String getUID() {
-        return UID;
-    }
-
-    public void setUID(String UID) {
-        this.UID = UID;
-    }
-
-    public String getBimage() {
-        return Bimage;
-    }
-
-    public void setBimage(String bimage) {
-        Bimage = bimage;
-    }
-
-    public String getBtype() {
-        return Btype;
-    }
-
-    public void setBtype(String btype) {
-        Btype = btype;
-    }
-
-    public String getBdiscription() {
-        return Bdiscription;
-    }
-
-    public void setBdiscription(String bdiscription) {
-        Bdiscription = bdiscription;
-    }
-
-    public String getBprice() {
-        return Bprice;
-    }
-
-    public void setBprice(String bprice) {
-        Bprice = bprice;
-    }
-
-    public Date getBtime() {
-        return Btime;
-    }
-
-    public void setBtime(Date btime) {
-        Btime = btime;
-    }
-
-    public boolean isBstatus() {
-        return Bstatus;
-    }
-
-    public void setBstatus(boolean bstatus) {
-        Bstatus = bstatus;
-    }
 }
